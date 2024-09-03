99ca0ad8a12d555f9f857015233862b7359f92fc
Thilak <vinaythilak002@gmail.com>
2024-09-03 15:09:10 +0530

    [style] error 401 page

--- client/src/pages/Error404Page.js
+++ client/src/pages/Error404Page.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-
+import { Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import FooterComponent from "../components/FooterComponent";
 
 const Error404Page = () => {
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate("/");
+    };
+
     return (
         <>
-            <Container className="flex-fill d-flex flex-column align-items-center justify-content-center">
-                <h1>ERROR 404 | PAGE NOT FOUND</h1>
+            <Container className="flex-fill d-flex flex-column align-items-center justify-content-center text-center">
+                <h1>ERROR 401 | Unauthorized</h1>
+                <p>You do not have permission to access this page.</p>
+                <Button variant="primary" onClick={handleGoHome}>
+                    Go to Home
+                </Button>
             </Container>
             <FooterComponent />
         </>
